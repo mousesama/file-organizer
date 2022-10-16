f50bfcc9a8c401f51a7d005839f8d755931422ed
BestLux <55964117+bestlux@users.noreply.github.com>
2022-10-16 01:11:28 -0700

    Configure Initial Requirements (#3)
    Dependency Injection
    Services
    Base View Model
    Initial View & VM

--- src/FIleOrganizer.Core/App.xaml.cs
+++ src/FIleOrganizer.Core/App.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using FileOrganizer.Core.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace FIleOrganizer.Core
 {
@@ -13,5 +10,30 @@ namespace FIleOrganizer.Core
     /// </summary>
     public partial class App : Application
     {
+        public static IHost? AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+            var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+            startupForm.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e);
+        }
     }
 }
---
+++ src/FIleOrganizer.Core/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using FIleOrganizer.Core.ViewModels;
+
+namespace FileOrganizer.Core.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindowVM ViewModel => (MainWindowVM)Resources["ViewModel"];
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel.Initialize();
+            Loaded += delegate { ViewModel.CallOnLoaded(); };
+            Unloaded += delegate { ViewModel.CallOnUnloaded(); };
+        }
+    }
+}
